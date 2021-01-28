6b3ffb333e4d0550a528700f0eb881cfbbbc8fd0
Vladimir Drobyshev <dvvinfo@mail.ru>
2021-01-28 19:21:02 +0300

    initializing the project

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="wrapper-content wrapper-content--fixed">
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        blog
+      </h1>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+
+</style>
