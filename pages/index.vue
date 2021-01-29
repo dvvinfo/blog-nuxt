b76db5299aa56f4eb46e7a5bc14227a8c097d78a
Vladimir Drobyshev <dvvinfo@mail.ru>
2021-01-29 13:47:29 +0300

    added the output of posts

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,43 @@
 <div class="wrapper-content wrapper-content--fixed">
   <promo/>
   <intro title="My last posts:"/>
+  <postList :posts="posts"/>
   </div>
 </template>
 
 <script>
 import promo from '@/components/Promo.vue'
 import intro from '@/components/UI/Intro.vue'
+import postList from '@/components/Blog/PostList.vue'
 export default {
   components: {
     promo,
-    intro
+    intro,
+   postList
+  },
+  data () {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: '1 post',
+          descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          img: 'https://img1.goodfon.ru/original/800x480/8/1f/bugatti-veyron-super-sport-5255.jpg'
+        },
+        {
+          id: 2,
+          title: '2 post',
+          descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          img: 'https://avatars.mds.yandex.net/get-pdb/33827/203472834-mclaren-mp4-12c-1458130241.5/s1200?webp=false'
+        },
+        {
+          id: 3,
+          title: '3 post',
+          descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          img: 'https://avatars.mds.yandex.net/get-pdb/33827/203472834-mclaren-mp4-12c-1458130241.5/s1200?webp=false'
+        },
+      ]
+    }
   }
 }
 </script>
