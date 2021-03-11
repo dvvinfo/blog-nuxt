bf22e245f9139cd28052923f84d7847033bd65ab
Vladimir Drobyshev <dvvinfo@mail.ru>
2021-03-11 20:23:08 +0300

    added adding articles

---
+++ components/Admin/NewPostForm.vue
@@ -0,0 +1,45 @@
+<template>
+    <section class="new-post">
+        <div class="container">
+            <form @submit.prevent>
+                <AppInput v-model="post.title"> Title:</AppInput>
+                <AppInput v-model="post.descr"> Descr:</AppInput>
+                <AppInput v-model="post.img"> Img link:</AppInput>
+                <AppTextArea v-model="post.content"> Content:</AppTextArea>
+                <!-- buttons -->
+                <div class="conrols">
+                    <AppButton class="btnDanger" @click="cancel">Cancel</AppButton>
+                    <AppButton @click="onSubmit">Save</AppButton>
+                </div>
+            </form>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            post: {
+                title: '',
+                descr: '',
+                img: '',
+                content: ''
+            }
+        }
+    },
+    methods: {
+        onSubmit () {
+            this.$emit('submit', this.post)
+        },
+        cancel () {
+            this.$router.push('/admin/')
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.conrols{
+    text-align: center;
+    margin: 20px auto;
+}
+</style>
---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <newPostForm @submit="onSubmit" />
+</template>
+<script>
+import newPostForm from '@/components/Admin/NewPostForm.vue'
+export default {
+    components: { newPostForm },
+    layout: 'admin',
+    methods: {
+       onSubmit (post) {
+           console.log('Post Added')
+           console.log(post)
+       }
+    }
+}
+</script>
